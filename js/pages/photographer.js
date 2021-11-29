d7f2a730a564dc783c12f8db0711f70b847853d0
Alex <collo@DESKTOP-0MTRO7E>
2021-11-29 16:22:17 +0100

    basic custom select without filter feature

--- js/pages/photographer.js
+++ js/pages/photographer.js
@@ -2,12 +2,13 @@ class Photographer {
     photographer
     constructor() {
         this.loadParams()
+        this.processSelect()
     }
 
     loadParams() {
         const params = new URLSearchParams(window.location.search)
         //TODO handle error
-        params.get('id') ? this.getPhotographer(Number(params.get('id'))) :null
+        params.get('id') ? this.getPhotographer(Number(params.get('id'))) : null
     }
 
     getPhotographer(id) {
@@ -36,6 +37,131 @@ class Photographer {
         const ctnAvatar = document.getElementById('photographerAvatar')
         ctnAvatar.appendChild(avatar)
     }
+
+    processSelect() {
+        
+        let x, i, j, l, ll, selElmnt, a, b, c;
+        /*look for any elements with the class "custom-select":*/
+        x = document.getElementsByClassName("custom-select");
+        l = x.length;
+        for (i = 0; i < l; i++) {
+            selElmnt = x[i].getElementsByTagName("select")[0];
+            ll = selElmnt.length;
+            /*for each element, create a new DIV that will act as the selected item:*/
+            a = document.createElement("DIV");
+            a.setAttribute("class", "select-selected");
+            a.innerHTML = selElmnt.options[selElmnt.selectedIndex].innerHTML;
+            x[i].appendChild(a);
+            /*for each element, create a new DIV that will contain the option list:*/
+            b = document.createElement("DIV");
+            b.setAttribute("class", "select-items select-hide");
+            for (j = 1; j < ll; j++) {
+                /*for each option in the original select element,
+                create a new DIV that will act as an option item:*/
+                c = document.createElement("DIV");
+                c.innerHTML = selElmnt.options[j].innerHTML;
+                c.addEventListener("click", function (e) {
+                    /*when an item is clicked, update the original select box,
+                    and the selected item:*/
+                    let y, i, k, s, h, sl, yl;
+                    s = this.parentNode.parentNode.getElementsByTagName("select")[0];
+                    sl = s.length;
+                    h = this.parentNode.previousSibling;
+                    
+                    for (i = 0; i < sl; i++) {
+                        if (s.options[i].innerHTML == this.innerHTML) {
+                            s.selectedIndex = i;
+                            h.innerHTML = this.innerHTML;
+                            
+                            y = this.parentNode.getElementsByClassName("same-as-selected");
+                            
+                            yl = y.length;
+                            for (k = 0; k < yl; k++) {
+                                //we display again last selected value
+                                y[k].style.display = "flex"
+                                y[k].removeAttribute("class");
+                               
+                            }
+                            this.setAttribute("class", "same-as-selected");
+                            //we hide picked value
+                            this.style.display = "none"
+                            break;
+                        }
+                    }
+                    h.click();
+                });
+                b.appendChild(c);
+            }
+            x[i].appendChild(b);
+            a.addEventListener("click", function (e) {
+                /*when the select box is clicked, close any other select boxes,
+                and open/close the current select box:*/
+                //change icon when open/close select
+                const selectInp = document.getElementsByClassName('select-selected')[0]
+                if ( selectInp.style.backgroundImage === 'url("../../assets/selectArrow_opened.png")') {
+                    selectInp.style.backgroundImage = "url('../../assets/selectArrow.png')"
+                    selectInp.style.borderBottomLeftRadius = "5px"
+                    selectInp.style.borderBottomRightRadius = "5px"
+                } else {
+                    selectInp.style.backgroundImage = "url('../../assets/selectArrow_opened.png')"
+                    selectInp.style.borderBottomLeftRadius = "0px"
+                    selectInp.style.borderBottomRightRadius = "0px"
+                }
+                 
+                e.stopPropagation();
+                /*a function that will close all select boxes in the document,
+        except the current select box:*/
+                let x, y, i, xl, yl, arrNo = [];
+                x = document.getElementsByClassName("select-items");
+                y = document.getElementsByClassName("select-selected");
+                xl = x.length;
+                yl = y.length;
+                for (i = 0; i < yl; i++) {
+                    if (this == y[i]) {
+                        arrNo.push(i)
+                    } else {
+                        y[i].classList.remove("select-arrow-active");
+                    }
+                }
+                for (i = 0; i < xl; i++) {
+                    if (arrNo.indexOf(i)) {
+                        x[i].classList.add("select-hide");
+                    }
+                }
+                this.nextSibling.classList.toggle("select-hide");
+                this.classList.toggle("select-arrow-active");
+                
+                
+            });
+            /*if the user clicks anywhere outside the select box,
+then close all select boxes:*/
+            document.addEventListener("click", this.closeAllSelect());
+        }
+    }
+
+
+    closeAllSelect(elmnt) {
+        /*a function that will close all select boxes in the document,
+        except the current select box:*/
+        let x, y, i, xl, yl, arrNo = [];
+        x = document.getElementsByClassName("select-items");
+        y = document.getElementsByClassName("select-selected");
+        xl = x.length;
+        yl = y.length;
+        for (i = 0; i < yl; i++) {
+            if (elmnt == y[i]) {
+                arrNo.push(i)
+            } else {
+                y[i].classList.remove("select-arrow-active");
+            }
+        }
+        for (i = 0; i < xl; i++) {
+            if (arrNo.indexOf(i)) {
+                x[i].classList.add("select-hide");
+            }
+        }
+    }
+
 
 }
 let photographerService = new PhotographerService()
