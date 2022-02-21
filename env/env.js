fff2b5d2250cf855ebfd33de1635b9f3839d3ca9
FrSenpai <Collosh@hotmail.fr>
2022-02-21 23:13:13 +0100

    woups, forgot to change env const lmao

--- env/env.js
+++ env/env.js
@@ -1,2 +1,1 @@
-// export const url = "https://frsenpai.github.io/Alexandreparjouet_6_13092021/"
-export const url = "http://localhost:5500/"
+export const url = "https://frsenpai.github.io/Alexandreparjouet_6_13092021/"
